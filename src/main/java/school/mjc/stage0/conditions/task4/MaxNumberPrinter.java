d49f0694c6b036844bd927adfa0ee0073ef7afb5
Whocares <yuldashevmrb@gmail.com>
2022-09-04 23:37:12 +0500

    done

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,6 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        System.out.println(Math.max( first, Math.max(second, third)));
     }
 }
